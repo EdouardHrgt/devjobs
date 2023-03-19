8f7d0eb3e8621dfd0154c43facd9ce6db62a7f89
Edouard Herrengt <86005588+EdouardHrgt@users.noreply.github.com>
2023-03-20 00:07:44 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap');
+:root {
+  /* colors */
+  --clr-violet: hsl(27, 22%, 51%);
+  --clr-light-violet: hsl(235, 82%, 77%);
+  --clr-dark-blue: hsl(219, 29%, 14%);
+  --clr-midnight: hsl(220, 29%, 10%);
+  --clr-white: hsl(0, 0%, 100%);
+  --clr-light-grey: hsl(210, 22%, 96%);
+  --clr-grey: hsl(212, 23%, 69%);
+  --clr-dark-grey: hsl(214, 17%, 51%);
+
+  /* fonts size */
+  --xl-font: 28px;
+  --l-font: 24px;
+  --m-font: 20px;
+  --s-font: 14px;
+  --body-font: 16px;
+
+  /* fonts line height */
+  --xl-line: 34px;
+  --l-line: 29px;
+  --m-line: 24px;
+  --s-line: 18px;
+  --body-line: 26px;
+
+  /* font family */
+  --font: 'Kumbh Sans', sans-serif;
 }
 </style>
